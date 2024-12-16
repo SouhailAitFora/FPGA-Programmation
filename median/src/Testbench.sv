de94abe8c91efaffb65ee6a06695fcec59ab6d8b
sagouzal-23 <said.agouzal@telecom-paris.fr>
2024-12-17 00:17:51 +0300

    complétion de code de verification de le bon fonctionnement de MCE.sv

--- median/src/Testbench.sv
+++ median/src/Testbench.sv
@@ -1,12 +1,34 @@
 module test_MCE;
 
-logic [0:7] A, B;
-logic [0:7] MAX, MIN;
-logic i;
+logic [7:0] A, B;
+logic [7:0] MAX, MIN;
+int i = 0;
+
+logic [0:7] MAX_SIM  ;
+logic [0:7] MIN_SIM  ;
 
 MCE I_MCE(.A(A), .B(B), .MAX(MAX), .MIN(MIN));
 
-for i in 1 ... 1000
+always(*)
 begin
-    assign A = 
+    for (i=0; i<1000;i++)
+        begin
+            A = $random() ;
+            B = $random() ;
+            MAX_SIM = ( A-B > 0 ) ? A : B ; 
+            MIN_SIM = ( A-B > 0 ) ? A : B ;
+
+            if (MAX_SIM != MAX || MIN_SIM != MIN) 
+                begin
+                    @always @(*) 
+                        begin
+                            $stop() ; 
+                            $display("MAX_SIM = %u et MAX = %u && MIN_SIM = %u et MIN = %u", MAX_SIM,MAX,MIN_SIM,MIN);
+                            $finish();
+                        end
+
+                end
+        end
+    $finish();    
 end
+endmodule
