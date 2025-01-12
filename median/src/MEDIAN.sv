3072cb575fb93b7333053aabfe6fb288532b920d
Souhail Ait Fora <saitfora-23@tp-1a201-09.enst.fr>
2025-01-12 19:53:34 +0100

    MEDIAN done syntesis done but simulation don't work

--- median/src/MEDIAN.sv
+++ median/src/MEDIAN.sv
@@ -1,84 +1,100 @@
-module MEDIAN #(parameter WIDTH = 8 , N_PIXELS = 9)
-            (input  logic  DSI,
-             input  logic  CLK,
-             input  logic  nRST,
-             input  logic [WIDTH-1:0] DI,
-             output logic [WIDTH-1:0] DO,
-             output logic DSO
-             );
+module MEDIAN(input  logic  DSI,
+              input  logic  CLK,
+              input  logic  nRST,
+              input  logic [7:0] DI,
+              output logic [7:0] DO,
+              output logic DSO
+              );
 
-logic BYP,BYP_tri;
+logic operating_reg;
+logic DSI_ret;
+logic BYP;
 int counter_step ,counter_clk;
+logic [7:0] DI_ret;
 
-assign BYP = DSI ? 1 : BYP_tri ;
+MED MED1(.BYP(BYP)  ,  .DSI(DSI_ret),  .CLK(CLK),  .DI(DI_ret),  .DO(DO));
 
-always_ff@(posedge CLK, negedge nRST)
-begin
-    
-    if(!nRST)
-    begin
+// this block manages our counters
+always_ff@(posedge CLK or negedge nRST) begin
+
+    if(!nRST) begin
         counter_step <= 8;
-        counter_clk  <= 0;
-        BYP_tri      <= 0;
+        counter_clk <= 0;
     end
 
-    if (!DSI)
-    begin
-        counter_clk <= counter_clk + 1 ;
-        
-        if(counter_clk < counter_step)
-        begin
-            BYP_tri <= 0; 
+    else if (operating_reg) begin
+        if (counter_clk == 8 && counter_step > 4) begin
+            counter_step <= counter_step - 1;
+            counter_clk <= 0;
+        end
+        else if (counter_clk == 4 && counter_step == 4) begin 
+            counter_step <= 8;
+            counter_clk <= 0;
+        end
+        else begin 
+            counter_clk <= counter_clk + 1;
         end
         
-        else if (counter_clk == 9) 
-        begin
-
-            counter_step <= counter_step - 1 ;
-            counter_clk  <= 0 ;    
-
-            if (counter_step ==4 )
-            begin
-                DSO <= 1 ;
-            end    
-
-        end
-
-        else if(counter_step > 4)
-        begin
-            BYP_tri <= 1 ;    
-        end
-
-    end
-end 
-
-always@(negedge DSI)
-begin
-    @(posedge CLK);
-    for (i = N_PIXELS - 1; i > 4; i--) begin
-
-            BYP = 0;
-            for (j = 0; j < i; j++) @(posedge CLK);
-            
-            BYP = 1;
-            for (j = i; j < N_PIXELS; j++) @(posedge CLK);
     end
 
-    BYP = 0;
-    for (j = 0; j < ((N_PIXELS - 1) >> 1); j++) @(posedge CLK);
-    @(posedge CLK);
-    DSO = 1;
-    @(posedge CLK);
-    DSO = 0;
 end
 
-always@(posedge clk)
-begin
-    if(!DSI)
-    begin
+always_ff@(posedge CLK or negedge nRST) begin
+
+    if(!nRST) begin
+        DSI_ret <= 0;
+        operating_reg <= 0;
+    end
+    else begin 
+
+        //retarding inputs because the we are one cycle slow
+        DI_ret <= DI;
+        DSI_ret <= DSI;
+
+        // managing operating_reg control signal
+        if (DSI_ret && !DSI) begin
+            operating_reg <= 1;
+        end
+        else if (DSO) begin
+            operating_reg <= 0;
+        end 
+        else begin 
+            operating_reg <= operating_reg;
+        end
 
     end
-end 
-MED MED1(.BYP(BYP)  ,  .DSI(DSI),  .CLK(CLK),  .DI(DI),  .DO(DO));
+
+end
+
+always_comb begin
+    
+    // managing BYP
+    if(operating_reg) begin
+        if(counter_clk < counter_step) begin 
+            BYP = 0;
+        end
+        else if(counter_step > 4) begin 
+            BYP = 1;
+        end
+        else begin 
+            BYP = 0;
+        end
+    end
+    else if(DSI) begin
+        BYP = 1;
+    end
+    else begin
+        BYP = 0;
+    end
+
+    // managing DSO
+    if(counter_step == 4 && counter_clk == 4) begin
+        DSO = 1;
+    end
+    else begin
+        DSO = 0;
+    end
+
+end
 
 endmodule
