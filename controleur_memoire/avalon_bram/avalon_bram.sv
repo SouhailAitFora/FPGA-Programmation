423e63cbf42c8b03a685300e8cf2f891ca930603
Said Agouzal <said.agouzal@telecom-paris.fr>
2025-01-16 17:26:46 +0100

    avalon_bram.sv WIP

--- controleur_memoire/avalon_bram/avalon_bram.sv
+++ controleur_memoire/avalon_bram/avalon_bram.sv
@@ -9,54 +9,39 @@
 // (voir doc mnl_avalon_spec.pdf, page 17)
 `timescale 1ns/1ps
 `default_nettype none
-module avalon_bram #(parameter RAM_ADD_W = 8, BURSTCOUNT_W = 4 ) (
+module avalon_bram #(parameter RAM_ADD_W = 32, BURSTCOUNT_W = 4 ) (
       // Avalon  interface for an agent
       avalon_if.agent avalon_a
       );
       // a vous de jouer a partir d'ici
-      logic [31:0] size =  1 << (RAM_ADD_W-2)  ;
+      parameter size =  1 << (RAM_ADD_W) ;
       logic [31:0] new_adress;
       logic [7:0] mem_0 [size-1:0];
       logic [7:0] mem_1 [size-1:0];
       logic [7:0] mem_2 [size-1:0];
       logic [7:0] mem_3 [size-1:0];
-      assign new_adress = ((avalon_a.address - avalon_a.address[1:0])>>1) + 1 ;
+      assign new_adress = (avalon_a.address >>2);
 
       always_ff @(posedge avalon_a.clk or posedge avalon_a.reset) begin
             if(avalon_a.reset)begin
                   avalon_a.waitrequest <= 1 ;
+                  avalon_a.readdatavalid <= 1 ;
             end
             else
             begin
             if(avalon_a.read)begin
                   avalon_a.waitrequest   <= 1 ;
                   avalon_a.readdatavalid <= 1 ;
-                  if(avalon_a.address<=3)begin
-
-                        case (avalon_a.address)
-                              0 : avalon_a.readdata <= mem_0 [0];
-                              1 : avalon_a.readdata <= mem_1 [1];
-                              2 : avalon_a.readdata <= mem_2 [2];
-                              3 : avalon_a.readdata <= mem_3 [3];
-                        endcase
-
-                  end
-                  else begin
-                        case (avalon_a.address[1:0])
-                              0 : avalon_a.readdata <= mem_0[new_adress];
-                              1 : avalon_a.readdata <= mem_1[new_adress]; 
-                              2 : avalon_a.readdata <= mem_2[new_adress];
-                              3 : avalon_a.readdata <= mem_3[new_adress];
-                        endcase
-                  end
-                  
+                  avalon_a.readdata <= { mem_3[new_adress], mem_2[new_adress], mem_1[new_adress], mem_0[new_adress]} ;
             end
             else begin
                   avalon_a.waitrequest <= 0;
+                  avalon_a.readdatavalid <= 0 ;
             end
 
             if(avalon_a.waitrequest )begin
                   avalon_a.waitrequest<=0 ;
+                  avalon_a.readdatavalid <= 0 ;
 
 
             end
@@ -64,29 +49,81 @@ module avalon_bram #(parameter RAM_ADD_W = 8, BURSTCOUNT_W = 4 ) (
             
       end
 
-      always @(*)
+      always @(posedge avalon_a.clk)
       begin
             if (avalon_a.write) 
             begin
-                  if(avalon_a.address<=3)begin
-
-                        case (avalon_a.address)
-                              0 :  mem_0 [0] = avalon_a.writedata ;
-                              1 :  mem_1 [1] = avalon_a.writedata ;
-                              2 :  mem_2 [2] = avalon_a.writedata ;
-                              3 :  mem_3 [3] = avalon_a.writedata ;
-                        endcase
-
+            case (avalon_a.byteenable)
+                  
+                  4'b0001: mem_0[new_adress] <= avalon_a.writedata[7:0];        
+                  4'b0010: mem_1[new_adress] <= avalon_a.writedata[15:8];       
+                  4'b0100: mem_2[new_adress] <= avalon_a.writedata[23:16];      
+                  4'b1000: mem_3[new_adress] <= avalon_a.writedata[31:24];      
+                  
+                  4'b0011: begin 
+                        mem_0[new_adress] <= avalon_a.writedata[7:0];
+                        mem_1[new_adress] <= avalon_a.writedata[15:8];
                   end
-                  else begin
-                        case (avalon_a.address[1:0])
-                              0 :  mem_0[new_adress] = avalon_a.writedata;
-                              1 :  mem_1[new_adress] = avalon_a.writedata; 
-                              2 :  mem_2[new_adress] = avalon_a.writedata;
-                              3 :  mem_3[new_adress] = avalon_a.writedata;
-                        endcase
+                  4'b0101: begin 
+                        mem_0[new_adress] <= avalon_a.writedata[7:0];
+                        mem_2[new_adress] <= avalon_a.writedata[23:16];
+                  end
+                  4'b1001: begin 
+                        mem_0[new_adress] <= avalon_a.writedata[7:0];
+                        mem_3[new_adress] <= avalon_a.writedata[31:24];
+                  end
+                  4'b0110: begin 
+                        mem_1[new_adress] <= avalon_a.writedata[15:8];
+                        mem_2[new_adress] <= avalon_a.writedata[23:16];
+                  end
+                  4'b1010: begin 
+                        mem_1[new_adress] <= avalon_a.writedata[15:8];
+                        mem_3[new_adress] <= avalon_a.writedata[31:24];
+                  end
+                  4'b1100: begin 
+                        mem_2[new_adress] <= avalon_a.writedata[23:16];
+                        mem_3[new_adress] <= avalon_a.writedata[31:24];
+                  end
+
+                  
+                  4'b0111: begin 
+                        mem_0[new_adress] <= avalon_a.writedata[7:0];
+                        mem_1[new_adress] <= avalon_a.writedata[15:8];
+                        mem_2[new_adress] <= avalon_a.writedata[23:16];
+                  end
+                  4'b1011: begin 
+                        mem_0[new_adress] <= avalon_a.writedata[7:0];
+                        mem_1[new_adress] <= avalon_a.writedata[15:8];
+                        mem_3[new_adress] <= avalon_a.writedata[31:24];
+                  end
+                  4'b1101: begin 
+                        mem_0[new_adress] <= avalon_a.writedata[7:0];
+                        mem_2[new_adress] <= avalon_a.writedata[23:16];
+                        mem_3[new_adress] <= avalon_a.writedata[31:24];
+                  end
+                  4'b1110: begin 
+                        mem_1[new_adress] <= avalon_a.writedata[15:8];
+                        mem_2[new_adress] <= avalon_a.writedata[23:16];
+                        mem_3[new_adress] <= avalon_a.writedata[31:24];
+                  end
+
+                  4'b1111: begin 
+                        mem_0[new_adress] <= avalon_a.writedata[7:0];
+                        mem_1[new_adress] <= avalon_a.writedata[15:8];
+                        mem_2[new_adress] <= avalon_a.writedata[23:16];
+                        mem_3[new_adress] <= avalon_a.writedata[31:24];
+                  end
+
+                  
+                  default: begin
+                        mem_0[new_adress] <= mem_0[new_adress];
+                        mem_1[new_adress] <= mem_0[new_adress];
+                        mem_2[new_adress] <= mem_0[new_adress];
+                        mem_3[new_adress] <= mem_0[new_adress];
+                  end
+            endcase
             end
-      end
+
       end
 
 
