e30032193593f0360bf4030aca5db453b6fd0699
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-01-20 17:05:44 +0300

    test_banch of Top.sv untested.

--- controleur_video/SoCFPGA/tb_src/tb_Top.sv
+++ controleur_video/SoCFPGA/tb_src/tb_Top.sv
@@ -20,7 +20,39 @@ Top Top0(.*) ;
 //  Code élèves
 //////////////////////////////
 
+// generation of clock signal 
+always begin
+    FPGA_CLK1_50 = 1 ;
+    # 10 ns 
+    FPGA_CLK1_50 = 0 ;
+    # 10 ns 
+end
 
+// simulation of an random interaction with KEY[0]
+initial begin
+
+    KEY[0] = 1 ;
+    #128 ns    ; 
+    KEY[0] = 0 ;
+    #128 ns    ;
+    KEY[0] = 1 ;
+
+end
+
+// end simulation after 4 ms
+always begin
+    # 4 ms ;
+    $stop(); 
+    
+end
+
+// test of LED[0]
+always begin
+    if(KEY[0] != LED[0]) begin
+        $display(" LED[0] != KEY[0]");
+        $stop();    
+    end
+end
 
 
 endmodule
